93b53916f679638d61e46874e85931b0646e806c
Rcgil30 <rcgil@uninorte.edu.co>
2023-04-19 19:03:34 -0500

    Implementación prim

--- app/Redraw.py
+++ app/Redraw.py
@@ -2,39 +2,21 @@ from main import grafo
 import pandas as pd
 import folium
 import os
-def Update_Map(start: str, finish: str):
-    if start == finish:
-        return
-    else:
-        vuelos = pd.read_csv('data/totalvuelos.csv')
-        map = folium.Map(location=[4.570868,-74.297333],zoom_start=6)
-        for index, location_info in vuelos.iterrows():
-            if location_info["Ciudad_Origen"] == start:
-                folium.Marker([location_info["lat_st"], location_info["lng_st"]], popup=location_info["Ciudad_Origen"], icon=folium.Icon(color="lightred", icon="plane")).add_to(map)
-            elif location_info["Ciudad_Origen"] == finish:
-                folium.Marker([location_info["lat_st"], location_info["lng_st"]], popup=location_info["Ciudad_Origen"], icon=folium.Icon(color="lightgreen", icon="plane")).add_to(map)
-            else:
-                folium.Marker([location_info["lat_st"], location_info["lng_st"]], popup=location_info["Ciudad_Origen"], icon=folium.Icon(color="pink", icon="plane")).add_to(map)
-        if finish == "TODOS":
-            lista = grafo.listavertices
-            for node in lista:
-                if node.data != start:
-                    lista2 = grafo.ListaRecorrido(start, node.data)
-                    for node1 in lista2:
-                        index = lista2.index(node1)
-                        if index != len(lista2) - 1:
-                            node2 = lista2[lista2.index(node1) + 1]
-                            weights = node1.weights[node1.connections.index(node2)]
-                            folium.vector_layers.PolyLine([(node1.lat, node1.long), (node2.lat, node2.long)], color="purple", weight=3, tooltip=str(weights)).add_to(map)
-        else:
-            lista = grafo.ListaRecorrido(start, finish)
-            for node in lista:
-                index = lista.index(node)
-                if index != len(lista) - 1:
-                    node2 = lista[lista.index(node) + 1]
-                    weights = node.weights[node.connections.index(node2)]
-                    folium.vector_layers.PolyLine([(node.lat, node.long), (node2.lat, node2.long)], color="purple", weight=3, tooltip=str(weights)).add_to(map)
-    
+def Update_Map(Aristas: list):
+
+    vuelos = pd.read_csv('data/totalvuelos.csv')
+    map = folium.Map(location=[4.570868,-74.297333],zoom_start=6)
+    for index, location_info in vuelos.iterrows():
+       folium.Marker([location_info["lat_st"], location_info["lng_st"]], popup=location_info["Ciudad_Origen"], icon=folium.Icon(color="pink", icon="plane")).add_to(map)
+
+    graph = grafo.MatrizAdy()
+
+    for i in range(len(Aristas)):
+        node1 = grafo.listavertices[i]
+        node2 = grafo.listavertices[Aristas[i]]
+        weight = graph[i][Aristas[i]]
+        folium.vector_layers.PolyLine([(node1.lat, node1.long), (node2.lat, node2.long)], color="purple", weight=3, tooltip=str(weight)).add_to(map)
+
     directory = r"app/static"
     Save = os.path.join(directory, "map.html")
     map.save(Save)
--- app/main.py
+++ app/main.py
@@ -4,16 +4,13 @@ import webbrowser
 import Redraw as R
 from flask import Flask, render_template, request
 import os
+import geopy.distance
+import sys
 """Creación del grafo"""
 class Node:
     def __init__(self, data: str) -> None:
         # Nombre de la ciudad
         self.data = data
-        # Lista de nodos a los cuales tiene conexión
-        self.connections: list[Node] = []
-        # Lista de pesos (Cada index corresponde al del nodo en connections)
-        self.weights: list[float] = []
-        # Posición que ocupa en la matriz de distancia
         self.pos = 0
         # Latitud y longitud de la ciudad
         self.lat: float = 0
@@ -26,76 +23,70 @@ class Grafo:
         # Cada elemento de la lista de vertices corresponde en su index a la de ciudades
         self.listavertices: list[Node] = []
         self.listaciudades: list[str] = []
-        self.MatrizDis: list[list[int]]
-        self.MatrizRec: list[list[Node]]
+        self.V = 0
 
-    def MatrizDistancia(self) -> list[list[int]]:
-        """Crea la matriz de distancia inicial (Si existe arista el peso, si no infinito)"""
-        Matriz = []
-        length = len(self.listavertices)
-        for i in range(length):
-            Fila = []
-            for j in range(length):
-                Fila.append(float("inf"))
-            Matriz.append(Fila)
-        for i in range(length):
-            Matriz[i][i] = 0
-        for vertice in self.listavertices:
-            for conexion in vertice.connections:
-                Matriz[vertice.pos][conexion.pos] = vertice.weights[vertice.connections.index(conexion)]
-        return Matriz
+    def MatrizAdy(self):
+        Matriz = [[0 for column in range(len(self.listaciudades))]
+                   for row in range(len(self.listaciudades))]
 
-    def MatrizRecorrido(self):
-        """Crea la matriz de recorrido inicial"""
-        Matriz = []
-        length = len(self.listavertices)
-        for i in range(length):
-            Fila = []
-            for j in range(length):
-                Fila.append(0)
-            Matriz.append(Fila)
-        for vertice in self.listavertices:
-            for i in range(length):
-                Matriz[i][vertice.pos] = vertice
-        
+        for node in self.listavertices:
+            for node2 in self.listavertices:
+                Matriz[node.pos][node2.pos] = geopy.distance.geodesic((node.lat, node.long), (node2.lat, node2.long)).km
+
         return Matriz
     
-    def FloydWarshall(self):
-        """Ejecuta el algoritmo de Floyd-Warshall, modifica la matriz de distancia y recorrido"""
-        n = len(self.listavertices)
-        Matriz = self.MatrizDistancia()
-        MatrizR = self.MatrizRecorrido()
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    Min = min(Matriz[i][j], Matriz[i][k] + Matriz[k][j])
-                    if Min != Matriz[i][j]:
-                        MatrizR[i][j] = self.listavertices[k]
-                    Matriz[i][j] = Min
-                    
-        self.MatrizDis = Matriz
-        self.MatrizRec = MatrizR
+    def minKey(self, key, mstSet):
+ 
+        # Initialize min value
+        min = sys.maxsize
+ 
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+ 
+        return min_index
+ 
+    # Function to construct and print MST for a graph
+    # represented using adjacency matrix representation
+    def primMST(self):
+ 
+        # Key values used to pick minimum weight edge in cut
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V  # Array to store constructed MST
+        # Make key 0 so that this vertex is picked as first vertex
+        key[0] = 0
+        mstSet = [False] * self.V
+ 
+        parent[0] = -1  # First node is always the root of
 
-    def ListaRecorrido(self, start: str, finish: str):
-        """Devuelve una lista ordenada con el recorrido más corto de un nodo a otro"""
-        node1 = self.listavertices[self.listaciudades.index(start)]
-        node2 = self.listavertices[self.listaciudades.index(finish)]
-        lista = [node1]
-        if node2 in node1.connections and self.MatrizRec[node1.pos][node2.pos] == node2:
-            lista.append(node2)
-        else:
-            aux = node2
-            path = []
-            while aux not in node1.connections:
-                aux = self.MatrizRec[node1.pos][aux.pos]
-                path.append(aux)
-            path.reverse()
-            for node in path:
-                lista.append(node)
-            lista.append(node2)
-
-        return lista
-
+        graph = self.MatrizAdy()
+        for cout in range(self.V):
+ 
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minKey(key, mstSet)
+ 
+            # Put the minimum distance vertex in
+            # the shortest path tree
+            mstSet[u] = True
+ 
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for v in range(self.V):
+ 
+                # graph[u][v] is non zero only for adjacent vertices of m
+                # mstSet[v] is false for vertices not yet included in MST
+                # Update the key only if graph[u][v] is smaller than key[v]
+                if graph[u][v] > 0 and mstSet[v] == False \
+                and key[v] > graph[u][v]:
+                    key[v] = graph[u][v]
+                    parent[v] = u
+ 
+        return parent
 
 grafo = Grafo()
 vuelos = pd.read_csv('data/totalvuelos.csv')
@@ -115,23 +106,9 @@ for index, city in vuelos.iterrows():
         nodo.long = float(city["lng_st"])
         c += 1
 
+grafo.V = len(grafo.listaciudades)
 
-for index, info in vuelos.iterrows():
-    """Iteramos nuevamente a lo largo del df para definir las adyacencias
-       entre los vértices"""
-    indexor = grafo.listaciudades.index(info["Ciudad_Origen"])
-    indexdes = grafo.listaciudades.index(info["Ciudad_Destino"])
-    
-    ciudad_or = grafo.listavertices[indexor]
-    ciudad_des = grafo.listavertices[indexdes]
-
-    ciudad_or.connections.append(ciudad_des)
-    ciudad_or.weights.append(round(info["distance_km"]))
-
-# Una vez tenemos todos los datos necesarios en el grafo, llamamos
-# el algorítmo de Floyd Warshall para tener la información del camino mínimo
-# entre los vértices
-grafo.FloydWarshall()
+AristasPrim = grafo.primMST()
 
 map = folium.Map(location=[4.570868,-74.297333],zoom_start=6)
 for index, location_info in vuelos.iterrows():
@@ -139,6 +116,7 @@ for index, location_info in vuelos.iterrows():
 
 # Guardamos el mapa en la carpeta requerida
 directory = r"app/static"
+
 Save = os.path.join(directory, "map.html")
 map.save(Save)
 #Servidor en Flask
@@ -157,7 +135,7 @@ def ciudades():
     ciudad1 = request.form['city-1']
     ciudad2 = request.form['city-2']
     #Redibujar el mapa
-    R.Update_Map(ciudad1, ciudad2)
+    R.Update_Map(AristasPrim)
     #Refrescar la pagina
     return render_template('index.html')
 if __name__ == '__main__':
